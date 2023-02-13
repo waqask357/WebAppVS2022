231f7ad3a8177a8e7b0ed7e101047102d4553a46
Waqas khan <thewaqas1@hotmail.com>
2023-02-13 22:35:01 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppVS2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
